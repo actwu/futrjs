35b1d1416c0e7f30b454faaee63268ba747da990
Laucsap <110190482+actwu@users.noreply.github.com>
2024-02-27 11:10:42 +0800

    Update f.js

--- f.js
+++ f.js
@@ -1,3 +1,4 @@
+
 // OWNED BY ACTWU
 // CREDIT ME IF YOU USE XDD
 function el(op) {
@@ -13,54 +14,54 @@ newElement.id = did;
 if (name == "p") {newElement.setAttribute("b", "")}
 else if (name == "box") {newElement.setAttribute("b", "")}
 newElement.setAttribute(at, "");
-parentElement.appendChild(newElement);
+parentElement.append(newElement);
 }
 
 function elClass(op) {
-  const {name, clas } = op;
+const {name, clas } = op;
 
-  const newElement = document.createElement(element);
-  newElement.classList = clas;
+const newElement = document.createElement(element);
+newElement.classList = clas;
 }
 
 function elAddclass(op) {
-  const {name, clas } = op;
+const {name, clas } = op;
 
-  const newElement = document.createElement(element);
-  newElement.classList.add(clas);
+const newElement = document.createElement(element);
+newElement.classList.add(clas);
 }
 
 function elRemClass(op) {
-  const {name, clas } = op;
+const {name, clas } = op;
 
-  const newElement = document.createElement(element);
-  newElement.classList.remove(clas);
+const newElement = document.createElement(element);
+newElement.classList.remove(clas);
 }
 
 function elTogClass(op) {
-  const {name, clas } = op;
+const {name, clas } = op;
 
-  const newElement = document.createElement(element);
-  newElement.classList.add(clas);
+const newElement = document.createElement(element);
+newElement.classList.add(clas);
 }
 
 function elAt(op) {
-  const {name, at, val } = op;
+const {name, at, val } = op;
 
-  const newElement = document.createElement(element);
-  newElement.setAttribute(at, val);
+const newElement = document.createElement(element);
+newElement.setAttribute(at, val);
 }
 
 function elRemAt(op) {
-  const {name, at, val } = op;
+const {name, at, val } = op;
 
-  const newElement = document.createElement(element);
-  newElement.removeAttribute(at, val);
+const newElement = document.createElement(element);
+newElement.removeAttribute(at, val);
 }
 
 function elId(op) {
-  const {name, did } = op;
+const {name, did } = op;
 
-  const newElement = document.createElement(element);
-  newElement.id = did;
+const newElement = document.createElement(element);
+newElement.id = did;
 }
