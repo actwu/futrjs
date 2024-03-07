ac55a089b67e060b39eea6020d9066e93cdb41f5
Laucsap <110190482+actwu@users.noreply.github.com>
2024-03-07 18:17:25 +0800

    Update f.js
    Fixed The bug when missing value.

--- f.js
+++ f.js
@@ -3,63 +3,75 @@
 // CREDIT ME IF YOU USE XDD
 function el(op) {
 
-const {name, par, val, clas, did, at} = op;
-const newElement = document.createElement(name);
-const parentElement = document.querySelector(par);
-newElement.textContent = val;
-newElement.innerText = val;
-newElement.value = val;
-newElement.classList = clas;
-newElement.id = did; 
-newElement.setAttribute(at, "");
-parentElement.append(newElement);
-}
+    const body = document.querySelector("body");
+    const {name, par, val, clas, did, at, atval} = op;
+    const newElement = document.createElement(name);
+    const parentElement = document.querySelector(par);
+    
+    if (val !== "" || val !== " ") { newElement.textContent = val; }
+    else { newElement.textContent = "None" }
+   
+    if (clas !== "" || clas !== " ") { newElement.classList = clas; }
+    
+    if (did !== "" || did !== " ") { newElement.id = did; }
+    else { newElement.id = Math.floor(Math.random() * 99); }
+  
+  
+    if (par !== "" || par !== "" || par) { parentElement.append(newElement); }
+    else { body.append(newElement); }
 
-function elClass(op) {
-const {name, clas } = op;
 
-const newElement = document.createElement(element);
-newElement.classList = clas;
-}
+    if (at !== "" || at !== " " || at) { newElement.setAttribute(at, atval); }
+    else {at = ""; newElement.setAttribute("none", "")}
 
-function elAddclass(op) {
-const {name, clas } = op;
-
-const newElement = document.createElement(element);
-newElement.classList.add(clas);
-}
-
-function elRemClass(op) {
-const {name, clas } = op;
-
-const newElement = document.createElement(element);
-newElement.classList.remove(clas);
-}
-
-function elTogClass(op) {
-const {name, clas } = op;
-
-const newElement = document.createElement(element);
-newElement.classList.add(clas);
-}
-
-function elAt(op) {
-const {name, at, val } = op;
-
-const newElement = document.createElement(element);
-newElement.setAttribute(at, val);
-}
-
-function elRemAt(op) {
-const {name, at, val } = op;
-
-const newElement = document.createElement(element);
-newElement.removeAttribute(at, val);
-}
-
-function elId(op) {
-const {name, did } = op;
-
-const newElement = document.createElement(element);
-newElement.id = did;
-}
+  }
+    
+    function elClass(op) {
+    const {name, clas } = op;
+    
+    const newElement = document.createElement(element);
+    newElement.classList = clas;
+    }
+    
+    function elAddclass(op) {
+    const {name, clas } = op;
+    
+    const newElement = document.createElement(element);
+    newElement.classList.add(clas);
+    }
+    
+    function elRemClass(op) {
+    const {name, clas } = op;
+    
+    const newElement = document.createElement(element);
+    newElement.classList.remove(clas);
+    }
+    
+    function elTogClass(op) {
+    const {name, clas } = op;
+    
+    const newElement = document.createElement(element);
+    newElement.classList.add(clas);
+    }
+    
+    function elAt(op) {
+    const {name, at, val } = op;
+    
+    const newElement = document.createElement(element);
+    newElement.setAttribute(at, val);
+    }
+    
+    function elRemAt(op) {
+    const {name, at, val } = op;
+    
+    const newElement = document.createElement(element);
+    newElement.removeAttribute(at, val);
+    }
+    
+    function elId(op) {
+    const {name, did } = op;
+    
+    const newElement = document.createElement(element);
+    newElement.id = did;
+    }
+    
