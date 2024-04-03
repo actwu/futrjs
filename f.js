e1808a2b78e635665cf5c9c079627801419d2d10
actwu <110190482+actwu@users.noreply.github.com>
2024-04-03 16:13:48 +0800

    Update f.js

--- f.js
+++ f.js
@@ -1,75 +1,75 @@
 
+
 // OWNED BY ACTWU
 // CREDIT ME IF YOU USE XDD
 function el(op) {
 
-    let HTMLbody = document.querySelector("body");
-    const {name, par, val, clas, did, at, atval} = op;
-    let newElement = document.createElement(name); 
-    const parentElement = document.querySelector(par);
+let HTMLbody = document.querySelector("body");
+const {name, par, val, clas, id, at, atval} = op;
+let newElement = document.createElement(name); 
+const parentElement = document.querySelector(par);
 
-    if (val !== "" || val !== " " || val) { newElement.textContent = val; }
-    else { newElement.textContent = "None" }
-   
-    if (clas !== "" || clas !== " " || clas) { newElement.classList = clas; }
-    
-    if (did !== "" || did !== " " || did) { newElement.id = did; }
-    else { newElement.id = Math.floor(Math.random() * 99); }
+if (val !== "" || val !== " " || val) { newElement.textContent = val; }
+else { newElement.textContent = "None" }
 
-    if (at !== "" || at !== " " || at) { newElement.setAttribute(at, atval); }
-    else { newElement.setAttribute("none", ""); }
+if (clas !== "" || clas !== " " || clas) { newElement.classList = clas; }
 
-    if(par === "" || par === " " || !par ) { HTMLbody.append(newElement); }
-    else { parentElement.append(newElement); }
+if (id !== "" || id !== " " || id) { newElement.id = id; }
+else { newElement.id = Math.floor(Math.random() * 99); }
 
-  }
-    
-    function elClass(op) {
-    const {name, clas } = op;
-    
-    const newElement = document.createElement(element);
-    newElement.classList = clas;
-    }
-    
-    function elAddclass(op) {
-    const {name, clas } = op;
-    
-    const newElement = document.createElement(element);
-    newElement.classList.add(clas);
-    }
-    
-    function elRemClass(op) {
-    const {name, clas } = op;
-    
-    const newElement = document.createElement(element);
-    newElement.classList.remove(clas);
-    }
-    
-    function elTogClass(op) {
-    const {name, clas } = op;
-    
-    const newElement = document.createElement(element);
-    newElement.classList.add(clas);
-    }
-    
-    function elAt(op) {
-    const {name, at, val } = op;
-    
-    const newElement = document.createElement(element);
-    newElement.setAttribute(at, val);
-    }
-    
-    function elRemAt(op) {
-    const {name, at, val } = op;
-    
-    const newElement = document.createElement(element);
-    newElement.removeAttribute(at, val);
-    }
-    
-    function elId(op) {
-    const {name, did } = op;
-    
-    const newElement = document.createElement(element);
-    newElement.id = did;
-    }
-    
+if (at !== "" || at !== " " || at) { newElement.setAttribute(at, atval); }
+else { newElement.setAttribute("none", ""); }
+
+if(par === "" || par === " " || !par ) { HTMLbody.append(newElement); }
+else { parentElement.append(newElement); }
+
+}
+
+function elClass(op) {
+const {name, clas } = op;
+
+const newElement = document.createElement(element);
+newElement.classList = clas;
+}
+
+function elAddclass(op) {
+const {name, clas } = op;
+
+const newElement = document.createElement(element);
+newElement.classList.add(clas);
+}
+
+function elRemClass(op) {
+const {name, clas } = op;
+
+const newElement = document.createElement(element);
+newElement.classList.remove(clas);
+}
+
+function elTogClass(op) {
+const {name, clas } = op;
+
+const newElement = document.createElement(element);
+newElement.classList.add(clas);
+}
+
+function elAt(op) {
+const {name, at, val } = op;
+
+const newElement = document.createElement(element);
+newElement.setAttribute(at, val);
+}
+
+function elRemAt(op) {
+const {name, at, val } = op;
+
+const newElement = document.createElement(element);
+newElement.removeAttribute(at, val);
+}
+
+function elId(op) {
+const {name, id } = op;
+
+const newElement = document.createElement(element);
+newElement.id = id;
+}
